afe2a5a9e2583ea0c4b5177c9557ca5ff3ea4de2
leedeokmu <freeefly@bluehole.net>
2019-07-26 18:30:39 +0900

    feat : step 6-2

--- src/App.js
+++ src/App.js
@@ -4,35 +4,19 @@ import Movie from './Movie'
 
 class App extends Component {
     state = {
-        greeting: 'Hello!',
     };
 
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                movies: [
-                    {
-                        title: "Matrix",
-                        poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
-                    }, {
-                        title: "Metal",
-                        poster: "https://posterspy.com/wp-content/uploads/2018/06/small-NEW-fmj-version-3-copy.jpg"
-                    }, {
-                        title: "Oldboy",
-                        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-                    }, {
-                        title: "Star Wars",
-                        poster: "https://cdn.images.express.co.uk/img/dynamic/36/590x/Star-Wars-actor-slams-The-Last-Jedi-1134707.jpg?r=1559328214519"
-                    }
-                ]
+        fetch('https://yts.lt/api/v2/list_movies.json?sort_by=download_count')
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log((err)));
+    }
 
-            });
-        }, 3000)
-    }
-     _renderMovies = () => {
-        const movies = this.state.movies.map((movie, index) => <Movie title={movie.title} poster={movie.poster} key={index}/>)
-         return movies;
-    }
+    _renderMovies = () => {
+        const movies = this.state.movies.map((movie, index) => <Movie title={movie.title} poster={movie.poster} key={index}/>);
+        return movies;
+    };
 
     render() {
         return (
--- src/Movie.js
+++ src/Movie.js
@@ -1,34 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 import './Movie.css';
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <MoviePoster poster={this.props.poster}/>
-            </div>
-        )
-    }
+const Movie = ({title, poster}) => {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <MoviePoster poster={poster}/>
+        </div>
+    )
+}
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
 }
 
-class MoviePoster extends Component {
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
+const MoviePoster = ({poster}) => {
+    return (
+        <img src={poster} alt=""/>
+    );
+}
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 
-    render() {
-        return (
-            <img src={this.props.poster} alt=""/>
-        );
-    }
 }
 
 export default Movie;
