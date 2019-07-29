744835d477fedfb2351538360e583599117bcb8a
leedeokmu <freeefly@bluehole.net>
2019-07-29 15:51:33 +0900

    feat : add css to Component

--- src/App.js
+++ src/App.js
@@ -3,25 +3,44 @@ import './App.css';
 import Movie from './Movie'
 
 class App extends Component {
-    state = {
-    };
+    state = {};
 
     componentDidMount() {
-        fetch('https://yts.lt/api/v2/list_movies.json?sort_by=download_count')
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch(err => console.log((err)));
+        this._getMovies();
     }
 
     _renderMovies = () => {
-        const movies = this.state.movies.map((movie, index) => <Movie title={movie.title} poster={movie.poster} key={index}/>);
+        const movies = this.state.movies.map(movie => {
+            return <Movie title={movie.title_english}
+                   poster={movie.medium_cover_image}
+                   genres={movie.genres}
+                   synopsis={movie.synopsis}
+                   key={movie.id}/>
+        });
         return movies;
     };
 
+    _getMovies = async () => {
+        const movies = await this._callApi()
+            .then(response => response.json())
+            .then(json => json.data.movies);
+
+        this.setState({
+            movies
+        })
+    }
+
+    _callApi = () => {
+        return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=download_count')
+            .then(response => response)
+            .catch(err => console.log((err)));
+    }
+
     render() {
+        const { movies } = this.state;
         return (
-            <div className="App">
-                {this.state.movies ? this._renderMovies() : 'Loading'}
+            <div className={movies ? "App" : "App--loading"}>
+                {movies ? this._renderMovies() : 'Loading'}
             </div>
         );
     }
--- src/Movie.js
+++ src/Movie.js
@@ -1,29 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import LinesEllipsis from 'react-lines-ellipsis'
 import './Movie.css';
 
-const Movie = ({title, poster}) => {
+const Movie = ({title, poster, genres, synopsis}) => {
     return (
-        <div>
-            <h1>{title}</h1>
-            <MoviePoster poster={poster}/>
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster} alt={title}/>
+            </div>
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+                </div>
+                <p className="Movie__Synopsis">
+                    <LinesEllipsis
+                        text={synopsis}
+                        maxLine='2'
+                        ellipsis='...'
+                        trimRight
+                        basedOn='letters'
+                    />
+                </p>
+            </div>
         </div>
-    )
-}
+    );
+};
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-}
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+};
 
-const MoviePoster = ({poster}) => {
+const MovieGenre = ({genre}) => {
     return (
-        <img src={poster} alt=""/>
-    );
-}
-MoviePoster.propTypes = {
-    poster: PropTypes.string.isRequired
+        <span className="Movie_Genre">{genre}</span>
+    )
+};
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
 
-}
+};
+
+const MoviePoster = ({poster, alt}) => {
+    return (
+        <img src={poster} className="Movie__Poster" alt={alt} title={alt}/>
+    );
+};
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+
+};
 
 export default Movie;
 
