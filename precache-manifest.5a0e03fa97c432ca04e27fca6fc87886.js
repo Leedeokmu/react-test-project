2042650cdf81b4ea50fdc63fbffd882008000a65
leedeokmu <freeefly@bluehole.net>
2019-07-29 16:25:31 +0900

    Updates

---
+++ precache-manifest.5a0e03fa97c432ca04e27fca6fc87886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827237f7f528bc76420838f15ea3a546",
+    "url": "/react-test-project/index.html"
+  },
+  {
+    "revision": "b6c51ece556572809ed7",
+    "url": "/react-test-project/static/css/main.1d10f888.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/react-test-project/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "b6c51ece556572809ed7",
+    "url": "/react-test-project/static/js/main.dd05e90d.chunk.js"
+  },
+  {
+    "revision": "80741f0b48d63e4395cf",
+    "url": "/react-test-project/static/js/runtime~main.2aa07f58.js"
+  }
+]);
