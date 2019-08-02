0e808baad2bc955b73f775318aabb6405b01c7e0
leedeokmu <freeefly@bluehole.net>
2019-08-02 09:24:48 +0900

    fix : change component class to function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class App extends Component{
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
-
+const App = () => {
+  return (
+    <div className="App">
+    </div>
+  );
 }
 
 export default App;
