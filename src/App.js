220359fdc4a9ed951ce788498f988b5dd4aeb20d
leedeokmu <freeefly@bluehole.net>
2019-08-26 17:58:18 +0900

    feat : add Home, Detail route (ing...)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {HashRouter, Route} from "react-router-dom";
+import client from './apolloClient'
+import { ApolloProvider } from '@apollo/react-hooks';
 
-const App = () => {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component{
+    render (){
+        return (
+            <ApolloProvider client={client}>
+                <HashRouter>
+                    <Route exact={true} path={"/"} component/>
+                </HashRouter>
+            </ApolloProvider>
+        );
+    }
 }
 
 export default App;
