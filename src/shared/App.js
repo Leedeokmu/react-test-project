0fd888ff75ed645dd824bd4fa96c1ced3ff760a7
freeefly <freefly3557@gmail.com>
2019-09-04 23:11:07 +0900

    feat : add asynRoute library
    resolve:

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import {Home, About, Posts} from "../pages";
+import Menu from "../components/Menu";
 
-function App() {
-  return (
-    <div>
-        <Route exact path="/" component={Home}/>
-        <Switch>
-            <Route path="/about/:name" component={About}/>
-            <Route path="/about" component={About}/>
-        </Switch>
-        <Route path="/posts" component={Posts}/>
-    </div>
-  );
+class App extends Component{
+    state = {
+        SplitMe: null
+    };
+    _handleClick = () => {
+        import('../components/SplitMe').then(({default: SplitMe}) => {
+            this.setState({
+                SplitMe
+            });
+        });
+    }
+
+    render() {
+        const {SplitMe} = this.state;
+        return (
+            <div>
+                <Menu/>
+                { SplitMe && <SplitMe/>}
+                <button onClick={this._handleClick}>Click This!</button>
+                <Route exact path="/" component={Home}/>
+                <Route path="/posts" component={Posts}/>
+                <Switch>
+                    <Route path="/about/:name" component={About}/>
+                    <Route path="/about" component={About}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
